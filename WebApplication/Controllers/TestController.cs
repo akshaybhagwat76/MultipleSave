1b10e006d421cf3cf1b40e4c332e9862a709fbcd
Akshay <akshay.bhagvat@viprak.com>
2018-06-04 17:57:23 -0700

    added project

---
+++ WebApplication/Controllers/TestController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.EDM;
+namespace WebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        demoEntities db = new demoEntities();
+        public ActionResult Index()
+        {
+            var random = RandomString(5);
+
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetData()
+        {
+            var data = (from cust in db.Customers
+                        from add in db.Addresses
+                        from pay in db.payments
+                        where add.c_id == cust.c_id && pay.c_id == cust.c_id
+                        select new
+                        {
+                            cust.c_id,
+                            add.add_id,
+                            pay.p_id,
+                            cust.cemail,
+                            add.c_address,
+                            pay.p_amount
+                        }).Distinct().ToList();
+
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult SaveMasterDetail(string c_email, string p_amount, string c_address, int c_id, int add_id, int p_id)
+        {
+            bool result = false;
+
+            if (c_id > 0)
+            {
+                var customer = db.Customers.Where(a => a.c_id == c_id).FirstOrDefault();
+                customer.c_id = c_id;
+                customer.cemail = c_email;
+                db.SaveChanges();
+            }
+            if (add_id > 0)
+            {
+                var add = db.Addresses.Where(a => a.add_id == add_id).FirstOrDefault();
+                add.c_address = c_address;
+                add.add_id = add_id;
+                db.SaveChanges();
+            }
+            if (p_id > 0)
+            {
+                var pay = db.payments.Where(a => a.p_id == p_id).FirstOrDefault();
+                pay.p_amount = Convert.ToDouble(p_amount);
+                pay.p_id = p_id;
+                db.SaveChanges();
+                result = true;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult SaveDetails(string cemail, string cmobile, Address[] addressObj, payment[] paymentObj)
+        {
+            bool result = false;
+            try
+            {
+                if (cemail != "" || cmobile != "")
+                {
+                    Customer objcust = new Customer();
+                    objcust.cemail = cemail;
+                    objcust.cmobile = cmobile;
+                    db.Customers.Add(objcust);
+                    db.SaveChanges();
+
+                    int LetstCustId = objcust.c_id;
+
+                    if (LetstCustId > 0 && addressObj.Length > 0 || addressObj != null)
+                    {
+                        foreach (var item in addressObj)
+                        {
+                            Address objadd = new Address();
+                            objadd.c_address = item.c_address;
+                            objadd.c_id = LetstCustId;
+                            db.Addresses.Add(objadd);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (paymentObj.Length > 0 && paymentObj != null)
+                    {
+                        foreach (var item in paymentObj)
+                        {
+                            payment objadd = new payment();
+                            objadd.p_amount = item.p_amount;
+                            objadd.c_id = LetstCustId;
+                            db.payments.Add(objadd);
+                            db.SaveChanges();
+                            result = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result);
+        }
+
+        private Random random = new Random((int)DateTime.Now.Ticks);
+        private string RandomString(int Size)
+        {
+            string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < Size; i++)
+            {
+                ch = input[random.Next(0, input.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ WebApplication/assets/main.js
@@ -0,0 +1,148 @@
+﻿var addressItem = [];
+var paymentItem = [];
+var acnt = 2;
+
+$(document).ready(function () {
+    loadCustomers();
+});
+
+function loadCustomers() {
+    $.ajax({
+        type: "GET",
+        url: "/Test/GetData",
+        dataType: "json",
+        success: function (response) {
+            let cnt = 1;
+            $.each(response, function (key, item) {
+                $('#mytbl').append('<tr><td class="editableColumns" id="email_row' + cnt + '">' + item.cemail + '</td><td class="editableColumns" id="amt_row' + cnt + '">' + item.p_amount + '</td><td class="editableColumns" id="address_row' + cnt + '">' + item.c_address + '</td><td><input class="editValues" id="edit_button' + cnt + '" onclick="editFunction(' + item.c_id + ',' + cnt + ')" type="button" value="Edit"><input class="saveValues" id="save_button' + cnt + '" onclick="saveFunction(' + item.c_id + ',' + cnt + ',' + item.add_id + ',' + item.p_id + ')" type="button" style="display:none"    value="SAVE"></td></tr>');
+                cnt++;
+            });
+        }
+    })
+}
+
+function editFunction(cust_id, rowno) {
+    document.getElementById("edit_button" + rowno).style.display = "none";
+    document.getElementById("save_button" + rowno).style.display = "block";
+
+    var emailr = document.getElementById("email_row" + rowno);
+    var amtr = document.getElementById("amt_row" + rowno);
+    var addrer = document.getElementById("address_row" + rowno);
+
+    var emailr_data = emailr.innerHTML;
+    var amtr_data = amtr.innerHTML;
+    var addrer_data = addrer.innerHTML;
+
+    emailr.innerHTML = "<input type='text' id='name_text" + rowno + "' value='" + emailr_data + "'>";
+    amtr.innerHTML = "<input type='number' id='country_text" + rowno + "' value='" + amtr_data + "'>";
+    addrer.innerHTML = "<input type='text' id='age_text" + rowno + "' value='" + addrer_data + "'>";
+
+};
+
+function saveFunction(cust_id, rowno, add_id, p_id) {
+    var name_val = document.getElementById("name_text" + rowno).value;
+    var country_val = document.getElementById("country_text" + rowno).value;
+    var age_val = document.getElementById("age_text" + rowno).value;
+
+    document.getElementById("email_row" + rowno).innerHTML = name_val;
+    document.getElementById("amt_row" + rowno).innerHTML = country_val;
+    document.getElementById("address_row" + rowno).innerHTML = age_val;
+
+  
+    $.ajax({
+        dataType: 'json',
+        type: "POST",
+        url: "/Test/SaveMasterDetail",
+        data: { c_email: name_val, p_amount: country_val, c_address: age_val, c_id: cust_id, add_id: add_id, p_id: p_id },
+        success: function (response) {
+            if (response) {
+                document.getElementById("edit_button" + rowno).style.display = "block";
+                document.getElementById("save_button" + rowno).style.display = "none";
+            }
+            else {
+                alert('Failed');
+            }
+        }
+    })
+}
+
+
+$('#btnAddress').click(function () {
+    //if ($(this).val() != "") {
+    //    var frsttxt = $('#txtadd').val();
+    //    var lstText = $("#tblAddress tr:first input").val();
+    //    var cnt = 0;
+    //    $('#tblAddress tbody').append('<tr><td><input type="text" id="txtadd" value="' + frsttxt + '" /></td><td><input type="button" id="btnAremove" onclick="removeBtn(this,' + cnt + ');" value="remove" style="width:80px" class="btn btn-danger" /></td></tr>')
+    //    $('#txtadd').val('');
+    //    cnt++;
+    //    addressItem.push({
+    //        add_id: 0,
+    //        c_address: frsttxt,
+    //        c_id: 0
+    //    })
+    //}
+    //else {
+    //    alert("Please enter address");
+    //}
+    debugger;
+    var newtextboxdiv = $(document.createElement('div')).attr('id', 'TextBoxDiv' + acnt);
+    newtextboxdiv.after().html('<br /><input type="text" name="textbox' + acnt + '" id="textbox' + acnt + '" value="' + $('#textbox1').val() + '" />&nbsp;&nbsp;&nbsp;<input type="button" id="btnAremove" onclick="removeBtn(this,' + acnt + ');" value="remove" style="width:80px" class="btn btn-danger" />');
+    newtextboxdiv.appendTo("#TextBoxesGroup");
+    addressItem.push({
+        add_id: 0,
+        c_address: $('#textbox1').val(),
+        c_id: 0
+    });
+    acnt++;
+    $('#textbox1').val("");
+});
+
+$('#btnpayment').click(function () {
+    //var closeadd = $(this).closest('input').html();
+    if ($(this).val() != "") {
+        let frstamt = $('#txtamt').val();
+        var lstText = $("#tblPayment tr:first input").val();
+        var cnt = 0;
+        $('#tblPayment tbody').append('<tr><td><input type="number" id="txtamt" value="' + frstamt + '" /></td><td><input type="button" id="btnPremove" onclick="removeBtnpmt(this,' + cnt + ');" value="remove" style="width:80px" class="btn btn-danger" /></td></tr>')
+        $('#txtamt').val('');
+        cnt++;
+        paymentItem.push({
+            p_id: 0,
+            p_amount: frstamt,
+            c_id: 0
+        })
+    }
+    else {
+        alert("please enter payment");
+    }
+});
+
+function removeBtn($this, cnt) {
+    //addressItem.splice($.inArray($($this).parents("tr").find('input').val(), addressItem), 1);
+    //$($this).parents('tr').remove();
+    addressItem.splice($.inArray($($this).parents("div").find('input').val(), addressItem), 1);
+    $($this).parent("div").remove()
+}
+
+function removeBtnpmt($this, cnt) {
+    paymentItem.splice($.inArray($($this).parents("tr").find('input').val(), paymentItem), 1);
+    $($this).parents('tr').remove();
+}
+
+$('.btnSaveOrder').click(function () {
+    $.ajax({
+        dataType: 'json',
+        type: "POST",
+        url: "/Test/SaveDetails",
+        data: { cemail: $('#email').val(), cmobile: $('#mobile').val(), addressObj: addressItem, paymentObj: paymentItem },
+        success: function (response) {
+            if (response) {
+                alert('Records Inserted Successfully');
+                location.reload();
+            }
+            else {
+                alert('Failed');
+            }
+        }
+    })
+});
